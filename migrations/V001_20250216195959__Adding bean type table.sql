1b2816754d27ab0b5c4819dd45fda0ebfa3a75e3
Keith Hughes <keith.hughes@bbd.co.za>
2025-02-16 20:03:14 +0200

    feat: removed bean type table create if not exist.

--- migrations/V001_20250216195959__Adding bean type table.sql
+++
@@ -1,7 +0,0 @@
-IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'bean_types')
-BEGIN
-    CREATE TABLE [bean_types] (
-      [bean_id] integer PRIMARY KEY NOT NULL IDENTITY(1, 1),
-      [name] varchar(100) NOT NULL
-);
-END;
